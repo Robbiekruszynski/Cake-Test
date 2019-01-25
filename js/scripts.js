d6a8203621798c8167f4fa5d156bde261b142345
Robbie Kruszynski and Tien Le <tien@>
2019-01-24 16:28:37 -0800

    rule the world

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,34 @@
 //buszzzz logic
 var numRead = function(yourNum){
-  var newLife = "";
+
+  // var newLife = "";
+  var numArray = [];
+  var newArray = [];
 
   if(isNaN(yourNum)) {
     return "Not a number!!"
     }
-  // for (var i = 0; i<numArray.length; i++){
-    if(parseInt(yourNum)%7 === 0){
-    return "life";
-    }
-  // return newLife;
-  var numArray = [];
-  var newArray = [];
 
   for (var j=0; j < yourNum; j++) {
     numArray.push(j + 1);
   }
-    // if(numArray)
 
   for (var i=0; i < yourNum; i++){
     if(numArray[i] == 5){
       newArray[i] = "cake";
 
-    } else if(numArray[i] ==3){
+    } else if(numArray[i] == 3){
       newArray[i] = "sofa";
+
+    } else if(numArray[i] % 7 == 0){
+      newArray[i] = "life";
 
     } else {
       newArray[i] = numArray[i];
     }
-    newLife = newArray;
+    // newLife = newArray;
   }
-  return newLife;
+  return newArray;
 
 };
 
